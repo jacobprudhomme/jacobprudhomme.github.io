a6db80806a577b7e557fcdc1570fc2961fca37e6
Jacob Prud'homme <2160185+jacobprudhomme@users.noreply.github.com>
2024-12-05 12:51:32 +0100

    Updated EU CV and added NA one

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementsByClassName("cv-link").addEventListener("click", function(event) {
-    if (!event.target.getAttribute("href")) {
-        event.preventDefault();
-        alert("This isn't available yet, but it will be here soon :)");
-    }
-});
